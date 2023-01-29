d631789b7595f14261cb196a5b23d5331ee80628
Mahesh Magarde <81670877+MaheshMagarde@users.noreply.github.com>
2023-01-29 14:04:38 +0530

    port

--- app.js
+++ app.js
@@ -140,8 +140,8 @@ app.get("/:page",(req,res)=>{
 
 
 
-
-app.listen(3000,()=>
+const port = process.env.PORT || 3000;
+app.listen(port,()=>
 {
     console.log("Server is Started at port:3000");
 })
